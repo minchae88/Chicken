01a71967c9be00b96c550b3b47d66cee4946c65e
유태근 <dbxorms4712@naver.com>
2018-04-23 18:15:16 +0900

    04.23 커밋

--- src/main/ChickenStore.java
+++ src/main/ChickenStore.java
@@ -10,46 +10,47 @@ import view.StockView;
 
 
 public class ChickenStore extends JFrame {
-  
-  OrderView order;
-  SalesView sales;
-  StockView stock;
-  JTabbedPane pane = new JTabbedPane();
-  
-  // 생성자
-  public ChickenStore() { 
-  // 각각의 화면을 관리하는 클래스 객체 생성
-    setSize(1000,600);
-    order = new OrderView(this);
-    sales = new SalesView(getWidth(), getHeight());//매출관리
-    stock = new StockView();//재고관리 
-      
-      pane.addTab("주 문", order);
-      pane.addTab("매 출", sales);
-      pane.addTab("재 고", stock);
 
-      // 탭 화면의 최초화면을 인덱스로 지정
-      pane.setSelectedIndex(0);
-  
-      // 화면크기지정
-      add("Center", pane);
-      setVisible(true);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-  }
-  
-  public void setTabIndex(int i){
-    pane.setSelectedIndex(i);
-  }
-  
-  public void visible(boolean flag) {
-    
-    pane.setEnabledAt(1, flag);
-    pane.setEnabledAt(2, flag);
-    
-  }
-  
-  public static void main(String[] args) {
-      ChickenStore chickenStore = new ChickenStore();
-      chickenStore.visible(false);
-}
+	OrderView order;
+	SalesView sales;
+	StockView stock;
+	JTabbedPane pane = new JTabbedPane();
+
+	// 생성자
+	public ChickenStore() { 
+		
+		// 각각의 화면을 관리하는 클래스 객체 생성
+		setSize(1000,600);
+		order = new OrderView(this);
+		sales = new SalesView(getWidth(), getHeight());//매출관리
+		stock = new StockView();//재고관리 
+
+		pane.addTab("주 문", order);
+		pane.addTab("매 출", sales);
+		pane.addTab("재 고", stock);
+
+		// 탭 화면의 최초화면을 인덱스로 지정
+		pane.setSelectedIndex(0);
+
+		// 화면크기지정
+		add("Center", pane);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+	}
+
+	public void setTabIndex(int i){
+		pane.setSelectedIndex(i);
+	}
+
+	public void visible(boolean flag) {
+
+		pane.setEnabledAt(1, flag);
+		pane.setEnabledAt(2, flag);
+
+	}
+
+	public static void main(String[] args) {
+		ChickenStore chickenStore = new ChickenStore();
+		chickenStore.visible(false);
+	}
 }
--- src/view/OrderDialog.java
+++ src/view/OrderDialog.java
@@ -1,42 +1,75 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Vector;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import model.OrderModel;
 
 public class OrderDialog extends JDialog implements ActionListener {
-	
+
 	JLabel title, orderNum, menuList, totalPrice;
 	JButton modify, cancle, payment;
 	JTextField orderField, priceField;
-	JLabel[] menuField, menuNum;
-	JButton[] plus, minus;
-	
-	public OrderDialog() {
-		
+	JButton plus = new JButton("+");
+	JButton minus = new JButton("-");
+	JTable tableOrderList;
+	DefaultTableModel tableModel;
+	Vector columnNames = new Vector();
+
+	OrderView parents;
+
+	OrderModel model;
+
+	// public OrderDialog(JTable tableOrderList) {
+	//
+	// this.tableOrderList = tableOrderList;
+	// addLayout();
+	// eventProc();
+	// setTotalPrice();
+	//
+	// }
+
+	public OrderDialog(OrderView parents) {
+		this.parents = parents;
 		addLayout();
 		eventProc();
-		
+		connectDB();
+		setTotalPrice();
+
 	}
-	
+
 	public ImageIcon getIcon(String name, int width, int height) {
-		
-		return new ImageIcon(new ImageIcon("src\\view\\chickimg\\"+name+".png").getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
+
+		return new ImageIcon(new ImageIcon("src\\view\\chickimg\\" + name + ".png").getImage().getScaledInstance(width,
+				height, Image.SCALE_DEFAULT));
 	}
-	
+
 	public void addLayout() {
-		
+
+		columnNames.add("주문번호");
+		columnNames.add("메뉴명");
+		columnNames.add("수량");
+		columnNames.add("가격");
+		tableModel = new DefaultTableModel(parents.data, columnNames);
+		tableOrderList = new JTable(tableModel);
+		setSize(500, 500);
+
 		title = new JLabel(getIcon("menuTitle", 200, 50));
 		orderNum = new JLabel(getIcon("orderNum", 100, 30));
 		menuList = new JLabel(getIcon("menuList", 100, 30));
@@ -52,70 +85,155 @@ public class OrderDialog extends JDialog implements ActionListener {
 		payment.setContentAreaFilled(false);
 		orderField = new JTextField();
 		priceField = new JTextField();
-		
+
 		JPanel p_north = new JPanel();
-		p_north.setLayout(new GridLayout(1,6));
+		p_north.setLayout(new GridLayout(1, 6));
 		p_north.add(title);
-		
+
 		JPanel p_center = new JPanel();
 		p_center.setLayout(new BorderLayout());
-		
+
 		JPanel p_center_north = new JPanel();
-		p_center_north.setLayout(new GridLayout(1,6));
+		p_center_north.setLayout(new GridLayout(1, 6));
 		p_center_north.add(orderNum);
 		p_center_north.add(orderField);
-		for(int i=0; i<4; i++) {
+		for (int i = 0; i < 4; i++) {
 			p_center_north.add(new JLabel(" "));
 		}
-		
+
 		JPanel p_center_center = new JPanel();
-		p_center_center.setLayout(new GridLayout(5, 1));
-		p_center_center.add(new JLabel("a"));
-		p_center_center.add(new JLabel("b"));
-		p_center_center.add(new JLabel("c"));
-		p_center_center.add(new JLabel("a"));
-		p_center_center.add(new JLabel("b"));
-		p_center_center.add(new JLabel("c"));
-		p_center_center.add(new JLabel("a"));
-		p_center_center.add(new JLabel("b"));
-		p_center_center.add(new JLabel("c"));
-		p_center_center.add(new JLabel("a"));
-		p_center_center.add(new JLabel("b"));
-		p_center_center.add(new JLabel("c"));
-		
+		p_center_center.setLayout(new BorderLayout());
+		p_center_center.setPreferredSize(new Dimension(200, 300));
+		JScrollPane scrollPane = new JScrollPane(tableOrderList);
+		scrollPane.setPreferredSize(new Dimension(200, 300));
+		p_center_center.add(tableOrderList);
+
+		JPanel p_center_east = new JPanel();
+		p_center_east.setLayout(new GridLayout(2, 1));
+		p_center_east.add(plus);
+		p_center_east.add(minus);
+
 		JPanel p_center_south = new JPanel();
-		p_center_south.setLayout(new GridLayout(2,6));
+		p_center_south.setLayout(new GridLayout(2, 6));
 		p_center_south.add(totalPrice);
 		p_center_south.add(priceField);
-		for(int i=0; i<10; i++) {
+		for (int i = 0; i < 10; i++) {
 			p_center_south.add(new JLabel(" "));
 		}
-		
+
 		JPanel p_south = new JPanel();
 		p_south.add(modify);
 		p_south.add(cancle);
 		p_south.add(payment);
-	
+
 		setLayout(new BorderLayout());
 		p_center.add(p_center_north, BorderLayout.NORTH);
 		p_center.add(new JScrollPane(p_center_center), BorderLayout.CENTER);
+		p_center.add(p_center_east, BorderLayout.EAST);
 		p_center.add(p_center_south, BorderLayout.SOUTH);
 		add(p_center, BorderLayout.CENTER);
 		add(p_north, BorderLayout.NORTH);
 		add(p_south, BorderLayout.SOUTH);
-		
-		setSize(500,500);
+
 		setVisible(true);
 
 	}
-	
+
 	public void eventProc() {
-		
+
+		plus.addActionListener(this);
+		minus.addActionListener(this);
+		cancle.addActionListener(this);
+		payment.addActionListener(this);
+
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		
+
+		Object evt = e.getSource();
+		if (plus == evt) {
+			menuPlus();
+			setTotalPrice();
+		} else if (minus == evt) {
+			menuMinus();
+			setTotalPrice();
+		} else if (cancle == evt) {
+			tableModel.removeRow(tableOrderList.getSelectedRow());
+			setTotalPrice();
+		} else if (payment == evt) {
+			if (insertOrderList() == 0) {
+				parents.tableModel.setNumRows(0);
+				parents.tableModel.fireTableDataChanged();
+				this.dispose();
+			}
+		}
 	}
-	
-	
+
+	public void menuPlus() {
+
+		int row = tableOrderList.getSelectedRow();
+		int changeNum = Integer.parseInt(String.valueOf(tableOrderList.getValueAt(row, 2))) + 1;
+		tableOrderList.setValueAt(changeNum, row, 2);
+
+		if (changeNum < 1) {
+		} else {
+			int price = Integer.parseInt(String.valueOf(tableOrderList.getValueAt(row, 3))) / (changeNum - 1);
+			int changePrice = price * changeNum;
+			tableOrderList.setValueAt(changePrice, row, 3);
+		}
+
+	}
+
+	public void menuMinus() {
+
+		int row = tableOrderList.getSelectedRow();
+		int totalSum = Integer.parseInt(String.valueOf(tableOrderList.getValueAt(row, 3))); // 현재
+																							// 총
+																							// 금액
+		int currentNum = Integer.parseInt(String.valueOf(tableOrderList.getValueAt(row, 2))); // 현재
+																								// 갯수
+		int price = totalSum / currentNum; // 개당 단가
+		int changeNum = currentNum - 1;
+
+		if (changeNum < 1) {
+		} else {
+			tableOrderList.setValueAt(changeNum, row, 2);
+			tableOrderList.setValueAt(totalSum - price, row, 3);
+		}
+
+	}
+
+	public void setTotalPrice() {
+
+		int sum = 0;
+		for (int i = 0; i < tableModel.getRowCount(); i++)
+			sum += Integer.parseInt(String.valueOf(tableModel.getValueAt(i, 3)));
+
+		priceField.setText(String.valueOf(sum));
+	}
+
+	public void connectDB() {
+		try {
+			model = new OrderModel();
+			System.out.println("오더 디비 연결 성공");
+		} catch (Exception e) {
+			System.out.println("디비 연결 실패:" + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public int insertOrderList() {
+		int result = 0;
+		try {
+			model.getOid();
+			model.getEmpno();
+			result = model.insertOrderList(tableModel.getDataVector());
+		} catch (Exception e) {
+			System.out.println("주문 정보 전송 실패");
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
 }
